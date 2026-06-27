52791889586241d9b4fdb3721d0f717d2d8dfe10
Tle2797 <kuni3945585@gmail.com>
2026-06-27 12:52:00 +0700

    Initial commit

---
+++ Operator.java
@@ -0,0 +1,12 @@
+public class Operator {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 20;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+    public static void main(String[] args) {
+        int myNumber = 10;
+        String Mytext = "Tle";
+        boolean myBoolean = true;
+        float myFloat = 10.577f;
+        double myDouble = 10.6;
+        char myCar = 'Q';
+        int[] myArray = {1, 2, 3, 4, 5};
+
+
+        System.out.println("MyNumber: "+ myNumber);
+        System.out.println("Mytext: "+ Mytext);
+        System.out.println("myBoolean: " + myBoolean);
+        System.out.println("myFloat: "+ myFloat);
+        System.out.println("myDouble: "+ myDouble);
+        System.out.println("myCar: "+ myCar);
+        System.out.println("myArray: "+ myArray[1]);
+    }
+}
